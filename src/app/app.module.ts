9fa7850af5bdd20f938588f0d1ccf6dc51fef249
geltoncruz <gelton@midiaplural.com.br>
2018-03-30 11:25:20 -0300

    nouvelle page pour list les bieres en api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
@@ -6,27 +7,32 @@ import { MyApp } from './app.component';
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ItemDetailsPage } from '../pages/item-details/item-details';
 import { ListPage } from '../pages/list/list';
+import { TestPage } from './../pages/test/test';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    TestPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    TestPage
   ],
   providers: [
     StatusBar,
--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,32 @@
+import { NavController, Platform } from 'ionic-angular';
 import { Component } from '@angular/core';
+import { TestPage } from '../test/test';
+import { Http } from '@angular/http';
+import 'rxjs';
+
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  private basePath:string = '/cerva';
+  public bears: Array<{}>;
 
+  constructor(public navCtrl: NavController, public http: Http, public _platform:Platform ) {
+
+    if(this._platform.is('Cordova')){
+        this.basePath = 'http://184.171.245.138/~ladimacom/app/bears.json';
+    }
+
+    this.http.get(this.basePath + '/bears.json')
+              .map(res => res.json())
+              .subscribe(data =>{
+                this.bears = data;
+              });
+
+  }
+  goToTestePage(){
+    this.navCtrl.push(TestPage)
   }
 }
